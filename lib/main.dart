33cfc16c822e2cab798e927b49d612f16cdccecd
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-08-06 17:46:35 +0200

    Test text

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
-            Text('sample text to try CI'),
+            Text('THIS SHOULD NEVER BE VISIBLE IN PROD'),
             Text(
               'Jebałeś sie z Unity:',
               style: tt.headline3,
